63649db218243eeb81cf24eeed55857025264e34
Floyd McDonald <flmcdon@clarkson.edu>
2021-12-06 14:41:16 -0500

    l

--- ui.cpp
+++ ui.cpp
@@ -12,36 +12,44 @@ int main()
     int season_yr;
     string answer;
     
-    //dont think we need an option for height
+    
 
+cout << "Welcome to soccer team creator."; // Make this Start new season
 while(true){
-    cout << "(a)dd (b)egin_new_season (q)uit (s)earch (d)isplay (p)rint";// need to update
+    cout << "(a)dd (o)pen (q)uit (s)earch (d)isplay (b)egin_new_season";// need to update
     cin >> a;
-    // quit makes use of save function and returns 0
     cin.get();
-    switch (a) {
-      
-        //b for beginning , asking user for year
+    switch (a) { //Main
+        
+        //Need to write display statistics
+        //Need to write print
+        //Open
+        case 'o':
+        manip.open(); //not sure if we need an open switch... save essentially does this
+        break;
+
+        //b for beginning , asking user for year 
         case 'b': 
-       manip.begin_new_season(); // works
+        cout << "Would you like to start a new season?";
+        cin >> answer;
+        if (answer.substr(0) == "Y" || answer.substr(0) == "y")
+        cout << "What year is this season?";
+        cin >> season_yr;
         break;
 
-     // Add Player
+
+        // Add Player
         case 'a' :
-        manip.input_pair(cin); //works
+        manip.input_pair(cin); //Should add name to map
       break;
-
-        case 'p': 
-        manip.print();  //prints into a file //no need to put filename here 
-        break;
+        //Edit
+         case 'e':
+         break;
+        //Need to ask thorpe about this
 
         //Quit
         case 'q':
         return 0;
-        break;
-
-        case 'd':
-        manip.display();
         break;
 
         //Search
@@ -52,7 +60,7 @@ while(true){
         manip.search(target);
         while(true)
              {
-         cout << "(n)ext (p)revious (e)dit p(r)int e(x)it (s)top" ; //something like this probably more stuff
+         cout << "(n)ext (p)revious (e)dit p(r)int e(x)it" ; //something like this probably more stuff
         cin >> a;
         switch(a){
 
@@ -86,10 +94,5 @@ while(true){
     cout << static_cast<char> (12);
     /* error = */
     manip.display(); // most likely not needed here
-    if (error){
-       cout << "Can't find" << target << endl;
-       error = false;
-    }
 }
 
-}

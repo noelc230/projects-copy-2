4e2d94fbcf26747448cd93c592dc35af5f943d65
noelc230 <noelc230@gmail.com>
2021-12-07 22:38:39 -0500

    Changes

--- map.cpp
+++ map.cpp
@@ -2,7 +2,8 @@
 #include <map>                                                                       
 #include "classbuild.h"                                                              
 #include <fstream>                                                                   
-#include "map.h"                                                                     
+#include "map.h"
+
                                                                                      
 using namespace std;                                                                 
                                                                                      
@@ -13,7 +14,7 @@ bool mapmanip::save()         //need new UI to confirm why it is a bool
   ofstream out;                                                                      
   out.open(filename);   if (out.fail()) return true;                                                             
   // something like out<< m[key] << m[key].get_info_stuff_a_bunch_of_times           
-  for (auto itr = m.begin(); itr != m.end(); itr++)                                  
+  for (typename std::map<string,Student>::iterator itr = m.begin(); itr != m.end(); itr++)                                  
     {                                                                                
       cout << itr -> first << " "; //<< " " << itr -> second << " ";                 
       Student student = (itr -> second); //second is an object of class students and
@@ -24,8 +25,7 @@ bool mapmanip::save()         //need new UI to confirm why it is a bool
     }                                                                                
  // ask professor                                                                    
 }                                                                                    
-                                                                                     
-                                                                                     
+
 void mapmanip::input_pair(istream &in) // is fine                                           
 {                                                                                    
  Student student;                                                                    
@@ -40,101 +40,92 @@ student.regstatus();
     m.insert(pair <string, Student> (student.get_lname() + " " + student.get_fname(), student)); //the get_names return strings                      
  }
 
+void mapmanip :: search_map() {
+int YearBorn;
+string category;
+string registered;
+string name;
 
- void mapmanip :: jump (int l) {
-    if ( l > (m.size () + 1)) l = m.size() + 1;
-    if ( l < 0) l = 0;
-    top_line = pointer_line = l-1;
-}
-//dont think we need
+        cout << "Would you like to search by (n)ame, (c)ategory, (y)ear of birth, or (r)egistered"; // need to specify registered or not registered
+        char a;
+        cin >> a; 
 
-void mapmanip :: next ()
-{
-    if (pointer_line == (m.size() + 1)) return;
-    pointer_line--;
-    if ( pointer_line < top_line) top_line--;
-}
-// works fine .. should work in search
+    if(a == 'n'){
+        cin >> name;
 
-void mapmanip:: prev () {
-    if (pointer_line == 0) return;
-    pointer_line --;
-    if (pointer_line < top_line) top_line --;
-}
-//should work in search... is actually next
-
-/*void Buffer :: insert (const string &instring){
-    buff.insert (buff.begin() + pointer_line , instring); // map does not support an insert function... 
-}*/
-
-void mapmanip :: replace(const Student &student) {
-    auto itr = m.begin();
-     itr += (pointer_line - 1);
-    (itr -> second) = student; 
-                                                    }
-                           // map may not support a simple replace... most replace as object of the class classbuild 
-//could be extremely similar //do we need this?
-
-void mapmanip::erase() {
-    m.erase (m.begin() + (pointer_line-1));
-} // map supports some kind of erase .... may need to edit 
-
-void mapmanip :: search (string s) {
-    for (auto itr = m.begin(); itr != m.end(); itr++) { //maybe use iterator... itr = map.begin 
-    //.... through to map.end searching for string index(most likely first name)
-        //if (buff[i].find(s) != string :: npos)
-         {if ((itr -> first) != m.find(s))
-            top_line = 1;
-            return 0;
+        v = print_if(m.begin(), m.end(), if_name, cout, name);
+        while(true)
+        {
+cout << "Would you like to search by (c)ategory, (y)ear of birth, (r)egistered, or (d)isplay?";
+    cin >> a;
+     switch(a) {
+        case 'c':
+        v = print_if(v.begin(), v.end(), if_Ux, cout, category);
+        break;
+        case 'y':
+        v = print_if(v.begin(), v.end(), if_YoB, cout, YearBorn);
+        break;
+        case 'r':
+        v = print_if(v.begin(), v.end(), if_regstatus, cout, registered);
+        break;
+        case 'd':
+        return;
+        default:
+        break;
+     }
         }
     }
-    return 1;
-} //needed but will be somewhat different.... will search by key
-// need to have an output parameter to display 
-// probably wont return 1 or 0
+    if(a == 'y')
+        cin >> YearBorn;
 
-bool mapmanip::open () {
-    string line;
-    ifstream instr;
-    m.clear();
-    cout << "Filename:";
-    cin >> filename;
-    instr.open(filename);
-    while (getline (instr, line)) buff.push_back(line); // cant do that right now
-    instr.close();
-    strcpy ( fname, filename); // can't do this either
-}
-// look at output into file... maybe highjack code
-
-/*bool mapmanip::save ( {
-    ofstream out;
-    out.open(filename);
-    if (out.fail()) return true;
-    for (string s: buff) out << s << endl;
-    out.close();
-    return false;
-}
- // again highjacks output into file...
-*/
-
-void mapmanip :: display() {
-    int i;
-    int maxline;
-    maxline = top_line + height;
-    if (maxline > m.size()) maxline = m.size();
-
-    cout << long_dash << endl; //need longdash back... not sure where it went
-    for ( int i = top_line; i < maxline; i++) 
-    {
-        if (i == pointer_line) cout << " > "; else cout << " ";
-        if ( i< 9) cout << " " ;
-        auto itr = m.begin() + i;
-        Student student = (itr -> second);
-        cout << i+1 << " " ; student.getYoB(); student.getUx(); student.getreg_status(); << endl;
+        v = print_if(m.begin(), m.end(), if_YoB, cout, name);
+        while(true)
+        {
+cout << "Would you like to search by (c)ategory, (n)ame, (r)egistered, or (d)isplay?";
+    cin >> a;
+     switch(a) {
+        case 'c':
+        v = print_if(v.begin(), v.end(), if_Ux, cout, category);
+        break;
+        case 'n':
+        v = print_if(v.begin(), v.end(), if_name, cout, YearBorn);
+        break;
+        case 'r':
+        v = print_if(v.begin(), v.end(), if_regstatus, cout, registered);
+        break;
+        case 'd':
+        return;
+        default:
+        break;
+     }
+        }
+    if(a == 'c')
+        {
+        cin >> category;
+        v = print_if(m.begin(), m.end(), if_Ux, cout, category);
+        while(true)
+        {
+cout << "Would you like to search by (c)ategory, (n)ame, (r)egistered, or (d)isplay?";
+    cin >> a;
+     switch(a) {
+        case 'c':
+        v = print_if(v.begin(), v.end(), if_Ux, cout, category);
+        break;
+        case 'n':
+        v = print_if(v.begin(), v.end(), if_name, cout, YearBorn);
+        break;
+        case 'r':
+        v = print_if(v.begin(), v.end(), if_regstatus, cout, registered);
+        break;
+        case 'd':
+        return;
+        default:
+        break;
+            }
+        }
     }
-    cout << long_dash << endl;
-    } // the really hard stuff
-
+}
+    
 void mapmanip::begin_new_season()
     {
         string answer;
@@ -149,3 +140,26 @@ void mapmanip::begin_new_season()
         else return;
 
     }
+
+bool mapmanip::if_name(Student student, std::string &name)
+{
+if (student.get_lname() == name) return 1;
+else return 0;
+
+}
+bool mapmanip::if_YoB(Student student, int YearofB)
+{
+if (student.getYoB() == YearofB) return 1;
+else return 0;
+}
+bool mapmanip::if_regstatus(Student student, std::string regstatus)
+{
+if ((student.getreg_status()).substr(0) == (regstatus.substr(0))) return 1;
+else return 0;
+}
+bool mapmanip::if_Ux(Student student, std::string &Ux1)
+{
+if (student.getUx() == Ux1) return 1;
+else return 0;
+}
+
--- map.h
+++ map.h
@@ -8,10 +8,12 @@ class mapmanip
 {                                                                                    
 public:                                                                              
                                                                                      
-  void input_pair(istream &in);                                                                 
+  void input_pair(std::istream &in);                                                                 
   void output();                                                           
-  void copymap() 
-  {std::map<std::string, Student> m1(m);}   
+  std::map<std::string, Student> copymap() //returns a map
+  {std::map<std::string, Student> m1;
+  m1=m;
+  return(m1);}   
 
         void begin_new_season();
         void next();
@@ -19,7 +21,7 @@ public:
         bool open (); // need to integrate with outputintofile function
         void display(); // display statistics 
         void set_height(int h) {height = h;}
-        bool search(std::string &s); // maybe use a binary predicate algorithm
+        void search_map(); // maybe use a binary predicate algorithm
         void up();
         void down();
         void jump(int l);
@@ -29,19 +31,46 @@ public:
         bool save ();  //most likely saves to a file must write
         //a function to recove file contents
         void search_print();
+        bool if_name(Student student, std::string &name);
+        bool if_YoB(Student student, int YoB);
+        bool if_regstatus(Student student, std::string regstatus);
+        bool if_Ux(Student student, std::string &Ux1);
 
   // make sure to make a .clear for screen when outputting file contents to dipplay  
+  
+ 
+// condition takes a function that returns a bool... this bool will activate the function
+// compares the 
+// what would the call look like print_if(m.begin(), m.end(), print_if(), out)
+// what i want in the printif statement is to compare function output 
 
-     template <class BinaryPredicate, class iterator>
-     void print_if(iterator start = m.begin(), iterator end = m.end(), BinaryPredicate &condition, ostream\
- &out)                                                                          
-{                                                                               
-  for(start; itr != stop; itr++)                                     
+template <class BinaryPredicate, class iterator, class T>
+std::vector<Student> mapmanip::print_if(iterator start, iterator stop, BinaryPredicate condition, ostream\
+ out, T item)         
+{   
+  std::vector<Student> v1;                                                           
+  for(typename std::map<string,Student>::itr = start; itr != stop; itr++)                                     
     {                                                                           
-      if(condition(itr->))                                                       
-        {cout << student.get_lname() + " " + student.get_fname() + " " + student.getUx()  + " " + student.getreg_status() << endl;}                                                          
-    }                                                                           
-}                      
+      if(condition((itr -> second), item))                                                       
+        {v1.push_back(itr->second)}                                                         
+    }         
+    return(v1);                                                                  
+}    
+
+template <class BinaryPredicate, class iterator, class T>
+std::vector<Student> mapmanip::print_if(iterator start, iterator stop, BinaryPredicate condition, ostream\
+ out, T item)         
+{   
+    std::vector<Student> v1;                                                       
+  for(itr = start; itr != stop; itr++)                                     
+    {                                                                           
+      if(condition((itr*), item))                                                       
+        {v1.push_back(itr*)}                                                         
+    }         
+    return(v1);                                                                  
+}
+
+std::vector<Student> v;
 
 private:                                                                             
   std::map<std::string, Student> m;                                                  
